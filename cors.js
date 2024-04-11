437152c8f59e318dc259221c4a79e448ccc04f00
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2024-04-11 20:09:25 +0300

    Update cors.js

--- cors.js
+++ cors.js
@@ -1,10 +1,8 @@
 const corsOptions = {
-  // Permite doar cereri de la această origine
   origin: "*",
-  // Permite doar metodele GET și POST
   methods: "GET,POST",
-  // Returnează un status 204 pentru cererile prefligth (OPTIONS)
   optionsSuccessStatus: 204,
+  exposedHeaders: ["Access-Control-Allow-Private-Network"],
 };
 
 module.exports = {
